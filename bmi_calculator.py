5100340ff6ca86be55ed029bc839a87fb5e7b211
amilhousecunningham <92339988+amilhousecunningham@users.noreply.github.com>
2022-09-21 19:22:50 -0400

    updated comment on line 5

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -2,7 +2,7 @@
 
 # BMI = (weight in kg / height in meters squared)
 
-# Imperial version: BMI * 703                                                     strip()
+# Imperial version: BMI * 703                                          
 
 def gather_info():
     height = float(input("What is your height? (inches or meters) "))
